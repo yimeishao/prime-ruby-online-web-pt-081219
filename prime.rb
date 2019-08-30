2f25d2fdaca9a889306acbabfedfba0bdb64922a
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 04:52:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@ def prime?(num)
  num.each do |x| 
    if x < 2 
      return false
-   else
-  range = 2..num-1
-  range.each do |x|
+   elsif
+  (2..num-1).each do |x|
     return false if num % x == 0
   else true
 end 
